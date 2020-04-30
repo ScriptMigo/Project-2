5bc5e0da52e440fa930ffbded04a5bd285bed03c
crimson225 <crimson225@gmail.com>
2020-04-30 13:17:31 -0400

    Scott saved us. Finally done.

--- Sean/app.js
+++ Sean/app.js
@@ -1,3 +1,72 @@
+function JSONToCSVConvertor(JSONData, ReportTitle, ShowLabel) {
+  //If JSONData is not an object then JSON.parse will parse the JSON string in an Object
+  var arrData = typeof JSONData != 'object' ? JSON.parse(JSONData) : JSONData;
+  var CSV = '';    
+  //Set Report title in first row or line
+  CSV += ReportTitle + '\r\n\n';
+  //This condition will generate the Label/Header
+  if (ShowLabel) {
+      var row = "";
+      //This loop will extract the label from 1st index of on array
+      for (var index in arrData[0]) {
+          //Now convert each value to string and comma-seprated
+          row += index + ',';
+      }
+      row = row.slice(0, -1);
+      //append Label row with line break
+      CSV += row + '\r\n';
+  }
+  //1st loop is to extract each row
+  for (var i = 0; i < arrData.length; i++) {
+      var row = "";
+      //2nd loop will extract each column and convert it in string comma-seprated
+      for (var index in arrData[i]) {
+          row += '"' + arrData[i][index] + '",';
+      }
+      row.slice(0, row.length - 1);
+      //add a line break after each row
+      CSV += row + '\r\n';
+  }
+  if (CSV == '') {        
+      alert("Invalid data");
+      return;
+  }   
+  //Generate a file name
+  var fileName = "MyReport_";
+  //this will remove the blank-spaces from the title and replace it with an underscore
+  fileName += ReportTitle.replace(/ /g,"_");   
+  //Initialize file format you want csv or xls
+  var uri = 'data:text/csv;charset=utf-8,' + escape(CSV);
+  // Now the little tricky part.
+  // you can use either>> window.open(uri);
+  // but this will not work in some browsers
+  // or you will not get the correct file extension    
+  //this trick will generate a temp <a /> tag
+  var link = document.createElement("a");    
+  link.href = uri;
+  //set the visibility hidden so it will not effect on your web-layout
+  link.style = "visibility:hidden";
+  link.download = fileName + ".csv";
+  //this part will append the anchor tag and remove it after automatic click
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
+fetch('http://home.unheard.org/api/v1.0/getData')
+.then((response) => {
+  return response.json();
+})
+.then((data) => {
+  ReportTitle = 'Blah';
+  ShowLabel = true
+  JSONToCSVConvertor(data, ReportTitle, ShowLabel)
+});
+
+
+
+//START of d3 Viz 
+
+
 var margin = {top: 5, right: 100, bottom: 30, left: 30},
     width = 1550 - margin.left - margin.right,
     height = 675 - margin.top - margin.bottom;
@@ -18,10 +87,9 @@ var svg = d3.select("#my_dataviz")
 var unemploymentKey = function(year){
   return `Rate${year}`
 }
-
-// d3.json(unemployAPI+2018, function(data) {
-d3.csv("JoinedData.csv", function(data) {
-
+d3.csv("MyReport_Blah3.csv", function(data) {
+// d3.csv("JoinedData.csv", function(data) {
+    
     // List of groups 
     var allGroup = ["2018", "2017", "2016", "2015", "2014","2013","2012","2011","2010","2009","2008","2007","2006","2005","2004","2003","2002","2001","2000"]
 
